bcdb519af37fc4158fcc955afb7d9c4faf84d315
pushkar1995 <pushkar.gautam1994@gmail.com>
2024-03-22 16:21:09 +0545

    Slick Slider implemented successfully

--- README.md
+++ README.md
@@ -1,8 +1,4 @@
-# React + Vite
+Carousel used Reference Link: https://react-slick.neostack.com/
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Tutorial Reference Link: https://www.youtube.com/watch?v=6YnryDjEGr8&t=482s
 
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,60 @@
-export default function App() {
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+/* eslint-disable react/jsx-key */
+function App() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
   return (
-    <h1 className="text-3xl text-red-600 font-bold underline">
-      Hello world! K cha
-    </h1>
+    <div className="w-3/4 m-auto">
+      <div className="mt-20">
+      <Slider {...settings}>
+        {data.map((d) => (
+          <div className="bg-white h-[450px] text-black rounded-xl">
+            <div className="h-56 rounded-t-xl bg-indigo-500 flex justify-center items-center">
+              <img src={d.img} alt="" className="h-44 w-44 rounded-full"/>
+            </div>
+
+            <div className="flex flex-col justify-center items-center gap-4 p-4">
+              <p className="text-xl font-semibold">{d.name}</p>
+              <p>{d.review}</p>
+              <button className="bg-indigo-500 text-white text-lg px-6 py-1 rounded-xl">Read More</button>
+            </div>
+          </div>
+        ))}
+        </Slider>
+      </div>
+    </div>
   )
 }
+
+const data = [
+  {
+    name: `Budget App`,
+    img: `/MyProjects/BudgetApp.png`,
+    review: `This is the review`
+  },
+  {
+    name: `Countries App`,
+    img: `/MyProjects/CountriesApp.png`,
+    review: `This is the review`
+  },
+  {
+    name: `Motorcycle Booking App`,
+    img: `/MyProjects/MotorcycleBooking.png`,
+    review: `This is the review`
+  },
+  {
+    name: `Space Traveller App`,
+    img: `/MyProjects/SpaceTravellerApp.png`,
+    review: `This is the review`
+  },
+]
+
+export default App;
